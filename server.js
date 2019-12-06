059281a35ce6564ee6afec6ef10f0eb978bd7135
Ezra Okuro <eookuro@gotanyalo.com>
2019-12-06 14:20:15 -0800

    first commit files

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+ 
+var app = express();
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/images', express.static(__dirname + '/public/images'));
+
+var server = app.listen(process.env.PORT || 5000)
